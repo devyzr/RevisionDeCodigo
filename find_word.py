13b6e1df605e2a66922af2ae59caad5744d7a6b9
Devyzr <daniel.e.verdin@gmail.com>
2018-05-25 17:46:17 -0500

    Autoformatted with Black
    Looks much nicer.

--- find_word.py
+++ find_word.py
@@ -7,13 +7,14 @@ import json
 from os.path import isfile, join
 
 
-class FindWord():
+class FindWord:
+
     def __init__(self):
         self.dirs = []
-        self.word = ''
-        self.filename = ''
+        self.word = ""
+        self.filename = ""
         self.case_insensitive = False
-        self.ext = ''
+        self.ext = ""
 
     def main(self, args):
         self.parse_args(args)
@@ -28,57 +29,92 @@ class FindWord():
         locations = self.get_locations(only_files)
 
         if len(locations):
-            if(self.print_lines):
+            if self.print_lines:
                 pass
             else:
                 for l in locations:
                     print(l)
         else:
-            print('Nothing found')
+            print("Nothing found")
             return None
 
     # Parses CLI arguments
     def parse_args(self, args):
-        argparse_text = ('A tool to search for a word or filename in a set of '
-                         'files. It scans the directory it\'s in recursively, '
-                         'which might be resource intensive. A word, filename,'
-                         ' regex or regex preset must be provided.')
+        argparse_text = (
+            "A tool to search for a word or filename in a set of "
+            "files. It scans the directory it's in recursively, "
+            "which might be resource intensive. A word, filename,"
+            " regex or regex preset must be provided."
+        )
         ap = argparse.ArgumentParser(argparse_text)
-        ap.add_argument('--w', metavar='WORD', help='The word to search for.',
-                        default='')
-        ap.add_argument('--f', metavar='FILENAME',
-                        help='A word we want to find in the filename.',
-                        default='')
+        ap.add_argument(
+            "--w", metavar="WORD", help="The word to search for.", default=""
+        )
+        ap.add_argument(
+            "--f",
+            metavar="FILENAME",
+            help="A word we want to find in the filename.",
+            default="",
+        )
         # ToDo: Add filename regex.
-        ap.add_argument('--regex', help='A regex to search for inside a file. '
-                        'Remember that the script searches line by line, '
-                        'meaning multi-line regexes won\'t work.',
-                        default='', metavar='REGULAR_EXPRESSION')
-        ap.add_argument('--regex_preset', help='A series of regex presets, '
-                        'currently has internal support for \'java_sql\','
-                        'additional presets can be loaded from the \'presets'
-                        '.json\' file. You can add presets as well. You can'
-                        'see the available presets by using \'print_presets\''
-                        ' as the preset name.',
-                        metavar='PRESET_NAME')
-        ap.add_argument('--ext', metavar='EXTENSION', help='The extension '
-                        'of the filetype we\'re searching. If not provided it '
-                        'will search ALL files, which will take longer and '
-                        'include binaries that you probably won\'t be able to '
-                        'read. \nIf you only want to search for words in a '
-                        'specific file, put the filename here.', default='')
-        ap.add_argument('--case_insensitive', help='Make the tool not care '
-                        'about the case of the word/file we\'re looking for.',
-                        default=False, action='store_const', const=True)
-        ap.add_argument('--dir', help='Search in the specified directory.',
-                        default='', metavar='PATH_NAME')
-        ap.add_argument('--ignore', help='A string we want to ignore in the '
-                        'filename, we can ignore many strings if we separate '
-                        'them with commas.', default='',
-                        metavar='IGNORE_STRING')
-        ap.add_argument('--print_lines', help='Print the line that coincides '
-                        'with the search term.', default=False,
-                        action='store_const', const=True)
+        ap.add_argument(
+            "--regex",
+            help="A regex to search for inside a file. "
+            "Remember that the script searches line by line, "
+            "meaning multi-line regexes won't work.",
+            default="",
+            metavar="REGULAR_EXPRESSION",
+        )
+        ap.add_argument(
+            "--regex_preset",
+            help="A series of regex presets, "
+            "currently has internal support for 'java_sql',"
+            "additional presets can be loaded from the 'presets"
+            ".json' file. You can add presets as well. You can"
+            "see the available presets by using 'print_presets'"
+            " as the preset name.",
+            metavar="PRESET_NAME",
+        )
+        ap.add_argument(
+            "--ext",
+            metavar="EXTENSION",
+            help="The extension "
+            "of the filetype we're searching. If not provided it "
+            "will search ALL files, which will take longer and "
+            "include binaries that you probably won't be able to "
+            "read. \nIf you only want to search for words in a "
+            "specific file, put the filename here.",
+            default="",
+        )
+        ap.add_argument(
+            "--case_insensitive",
+            help="Make the tool not care "
+            "about the case of the word/file we're looking for.",
+            default=False,
+            action="store_const",
+            const=True,
+        )
+        ap.add_argument(
+            "--dir",
+            help="Search in the specified directory.",
+            default="",
+            metavar="PATH_NAME",
+        )
+        ap.add_argument(
+            "--ignore",
+            help="A string we want to ignore in the "
+            "filename, we can ignore many strings if we separate "
+            "them with commas.",
+            default="",
+            metavar="IGNORE_STRING",
+        )
+        ap.add_argument(
+            "--print_lines",
+            help="Print the line that coincides " "with the search term.",
+            default=False,
+            action="store_const",
+            const=True,
+        )
 
         self.ap = ap
         args = ap.parse_args(args)
@@ -99,32 +135,32 @@ class FindWord():
             self.regex = False
 
         if args.ignore:
-            self.ignore = args.ignore.split(',')
+            self.ignore = args.ignore.split(",")
         else:
-            self.ignore = ['']
+            self.ignore = [""]
 
         # Check that a search term has been passed, if not print help and exit
-        if(not (self.word or self.filename or self.regex)):
+        if not (self.word or self.filename or self.regex):
             self.ap.print_help()
             return None
 
     # Gets the files of a directory, if no directory is passed gets root dir
-    def get_files(self, path='', extension=''):
+    def get_files(self, path="", extension=""):
         only_files = []
-        if(path):
+        if path:
             files = os.listdir(path)
         else:
             files = os.listdir()
 
         for f in files:
-            if(path):
+            if path:
                 f = join(path, f)
             # Checks if it's a file, if not appends to directories
             if isfile(f):
                 str_f = str(f)
                 # Check for extension if specified, if not we append the file
-                if(extension):
-                    if (extension in str_f):
+                if extension:
+                    if extension in str_f:
                         only_files.append(f)
                 else:
                     only_files.append(f)
@@ -134,8 +170,7 @@ class FindWord():
         return only_files
 
     # Search the files we have for filenames or words in the files
-    def get_locations(self, only_files, word='', filename='',
-                      case_insensitive=False):
+    def get_locations(self, only_files, word="", filename="", case_insensitive=False):
         # These if statements are used for tests and CLI use of method
         if word:
             self.word = word
@@ -147,18 +182,17 @@ class FindWord():
         # file is the whole filename including directory
         locations = []
         for file in only_files:
-            if self.ignore != ['']:
-                if any(bad_ext == file[-len(bad_ext):] for
-                        bad_ext in self.ignore):
+            if self.ignore != [""]:
+                if any(bad_ext == file[-len(bad_ext) :] for bad_ext in self.ignore):
                     continue
             base_loc = str(file)
-            if(self.case_insensitive):
+            if self.case_insensitive:
                 file = file.upper()
                 # Take out of method
                 self.filename = self.filename.upper()
                 self.word = self.word.upper()
             # Check if filename exists and if it's in file
-            if(self.filename and self.filename in file):
+            if self.filename and self.filename in file:
                 # Won't add file to list if there is a word.
                 # This is to avoid duplicates. Hacky, I know.
                 if not self.word:
@@ -166,31 +200,31 @@ class FindWord():
                     file_parts = file.split(os.sep)
                     if self.filename in file_parts[-1]:
                         locations.append(base_loc)
-            elif(self.filename and self.word):
+            elif self.filename and self.word:
                 continue
             # Check if the user queried for a word.
             x = 0
-            with open(file, 'r', encoding='latin-1') as o_file:
+            with open(file, "r", encoding="latin-1") as o_file:
                 self.prev_line = 0
                 for line in o_file:
                     unmodded_line = line
-                    if(self.case_insensitive):
+                    if self.case_insensitive:
                         line = line.upper()
                     # A counter to find the line
                     x += 1
                     # Check if word exists and if it's in the line.
-                    if(self.word and self.word in line):
-                        loc = base_loc + ':' + str(x)
+                    if self.word and self.word in line:
+                        loc = base_loc + ":" + str(x)
                         locations.append(loc)
-                        if(self.print_lines):
+                        if self.print_lines:
                             self.print_results(loc, unmodded_line, x)
 
                     # Check for regex matches
-                    if(self.regex):
-                        if(self.regex.search(line)):
-                            loc = base_loc + ':' + str(x)
+                    if self.regex:
+                        if self.regex.search(line):
+                            loc = base_loc + ":" + str(x)
                             locations.append(loc)
-                            if(self.print_lines):
+                            if self.print_lines:
                                 self.print_results(loc, unmodded_line, x)
 
         return locations
@@ -199,12 +233,12 @@ class FindWord():
     # the lines of results we find.
     def print_results(self, l_location, l_content, current_l):
         text = l_content.strip()
-        text = text.encode(sys.stdout.encoding, errors='replace')
+        text = text.encode(sys.stdout.encoding, errors="replace")
         line_difference = current_l - self.prev_line
-        if(self.prev_line != 0 and line_difference == 1):
+        if self.prev_line != 0 and line_difference == 1:
             print(text)
         else:
-            print('\n%s' % l_location)
+            print("\n%s" % l_location)
             print(text)
         self.prev_line = current_l
 
@@ -212,58 +246,60 @@ class FindWord():
         # A dict of presets that are commonly used, to avoid having to copy
         # presets.json
         # SQL words, separated for use in other langs.
-        sql_words = ('(select|SELECT|insert|INSERT|where|WHERE|delete|DELETE|'
-                     'update|UPDATE|from|FROM|join|JOIN|like|LIKE|upper\s*\(|'
-                     'UPPER\s*\(|order\ by|ORDER\ BY|case|CASE|when|WHEN|then|'
-                     'THEN|else|ELSE|ltrim|LTRIM|rtrim|RTRIM|and|AND|SYSDATE|'
-                     'sysdate|VALUES|values|SMALLINT|smallint|VARCHAR|varchar'
-                     'DECLARE|declare|DATETIME|datetime|MONEY|money)')
-        preset_dict = {'java_sql': '\".*' + sql_words + '.*\"',
-                       # High priority danger words for Python
-                       'danger_noodle': '(subprocess|system|popen|exec|eval|'
-                       'pickle|shelve|yaml|sqlite3|pypyodbc|jsonpickle|urlib2|'
-                       'socket|fork|kill|execfile|__import__|path|spawn|popen2'
-                       '|commands|input|get|post|(commands\.)*getoutput|'
-                       '(commands\.)*getoutput|(commands\.)*getstatus|'
-                       '(commands\.)*getstatusouput|compile|(cPickle\.)*load|'
-                       '(cPickle\.)*loads|(marshal\.)*load|(marshal\.)*loads|'
-                       '(os\.)*execl|(os\.)*execle|(os\.)*execlp|'
-                       '(os\.)*execlpe|(os\.)*execv|(os\.)*execve|'
-                       '(os\.)*execvp|(os\.)*execvpe|(os\.)*popen|'
-                       '(os\.)*popen2|(os\.)*popen3|(os\.)*popen4|'
-                       '(os\.)*spawnl|(os\.)*spawnle|(os\.)*spawnlp|'
-                       '(os\.)*spawnlpe|(os\.)*spawnv|(os\.)*spawnve|'
-                       '(os\.)*spawnvp|(os\.)*spawnvpe|(os\.)*startfile'
-                       '|(os\.)*system|(pickle\.)*load|(pickle\.)*loads|'
-                       '(popen2\.)*popen2|(popen2\.)*popen3|(popen2\.)*popen4|'
-                       'shelve\.open|(subprocess\.)*call|'
-                       '(subprocess\.)*check_call|(subprocess\.)*check_output|'
-                       '(subprocess\.)*Popen|(yaml\.)*load|requests)',
-                       # Low priority danger words for Python
-                       'not_so_danger_noodle': '(logging|print|open|tarfile|'
-                       'zipfile|file|getattr|setattr|delattr)'}
+        sql_words = (
+            "(select|SELECT|insert|INSERT|where|WHERE|delete|DELETE|"
+            "update|UPDATE|from|FROM|join|JOIN|like|LIKE|upper\s*\(|"
+            "UPPER\s*\(|order\ by|ORDER\ BY|case|CASE|when|WHEN|then|"
+            "THEN|else|ELSE|ltrim|LTRIM|rtrim|RTRIM|and|AND|SYSDATE|"
+            "sysdate|VALUES|values|SMALLINT|smallint|VARCHAR|varchar"
+            "DECLARE|declare|DATETIME|datetime|MONEY|money)"
+        )
+        preset_dict = {
+            "java_sql": '".*' + sql_words + '.*"',
+            # High priority danger words for Python
+            "danger_noodle": "(subprocess|system|popen|exec|eval|"
+            "pickle|shelve|yaml|sqlite3|pypyodbc|jsonpickle|urlib2|"
+            "socket|fork|kill|execfile|__import__|path|spawn|popen2"
+            "|commands|input|get|post|(commands\.)*getoutput|"
+            "(commands\.)*getoutput|(commands\.)*getstatus|"
+            "(commands\.)*getstatusouput|compile|(cPickle\.)*load|"
+            "(cPickle\.)*loads|(marshal\.)*load|(marshal\.)*loads|"
+            "(os\.)*execl|(os\.)*execle|(os\.)*execlp|"
+            "(os\.)*execlpe|(os\.)*execv|(os\.)*execve|"
+            "(os\.)*execvp|(os\.)*execvpe|(os\.)*popen|"
+            "(os\.)*popen2|(os\.)*popen3|(os\.)*popen4|"
+            "(os\.)*spawnl|(os\.)*spawnle|(os\.)*spawnlp|"
+            "(os\.)*spawnlpe|(os\.)*spawnv|(os\.)*spawnve|"
+            "(os\.)*spawnvp|(os\.)*spawnvpe|(os\.)*startfile"
+            "|(os\.)*system|(pickle\.)*load|(pickle\.)*loads|"
+            "(popen2\.)*popen2|(popen2\.)*popen3|(popen2\.)*popen4|"
+            "shelve\.open|(subprocess\.)*call|"
+            "(subprocess\.)*check_call|(subprocess\.)*check_output|"
+            "(subprocess\.)*Popen|(yaml\.)*load|requests)",
+            # Low priority danger words for Python
+            "not_so_danger_noodle": "(logging|print|open|tarfile|"
+            "zipfile|file|getattr|setattr|delattr)",
+        }
 
         preset_json = {}
-        json_filename = 'presets.json'
-        if (os.path.isfile(json_filename) and
-                os.stat(json_filename).st_size != 0):
-            with open(json_filename, 'r') as json_file:
+        json_filename = "presets.json"
+        if os.path.isfile(json_filename) and os.stat(json_filename).st_size != 0:
+            with open(json_filename, "r") as json_file:
                 preset_json = json.load(json_file)
             preset_dict = {**preset_dict, **preset_json}
 
-        if preset_option == 'print_presets':
-            print('Available presets are:')
+        if preset_option == "print_presets":
+            print("Available presets are:")
             for preset in preset_dict:
-                print('>>> %s' % preset)
+                print(">>> %s" % preset)
                 print(preset_dict[preset])
                 print()
             sys.exit()
 
         if preset_option not in preset_dict:
-            print('Coulnd\'t find \'%s\', available presets are:'
-                  % preset_option)
+            print("Coulnd't find '%s', available presets are:" % preset_option)
             for preset in preset_dict:
-                print('>>> %s' % preset)
+                print(">>> %s" % preset)
                 print(preset_dict[preset])
                 print()
             sys.exit()
@@ -273,6 +309,6 @@ class FindWord():
         return regex
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fw = FindWord()
     sys.exit(fw.main(sys.argv[1:]))
